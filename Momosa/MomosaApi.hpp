9995f3e7932d535e2e57d64183bbb115e366724d
Patrick Moore <patrickmoore@gmail.com>
2015-02-04 15:14:08 -0500

    Fixed old name

--- Momosa/MomosaApi.hpp
+++ Momosa/MomosaApi.hpp
@@ -16,17 +16,17 @@
 
 #pragma once
 
-#if defined(MOMONAV_DLL_EXPORTS)
-#   define MOMONAV_DLL_API __declspec(dllexport)
+#if defined(MOMOSA_DLL_EXPORTS)
+#   define MOMOSA_DLL_API __declspec(dllexport)
 #else
-#   define MOMONAV_DLL_API __declspec(dllimport)
+#   define MOMOSA_DLL_API __declspec(dllimport)
 #endif
 
 #include "point_search.h"
 
 extern "C" 
 {
-    MOMONAV_DLL_API SearchContext* create(const Point* points_begin, const Point* points_end);
-    MOMONAV_DLL_API int32_t search(SearchContext* sc, const Rect rect, const int32_t count, Point* out_points);
-    MOMONAV_DLL_API SearchContext* destroy(SearchContext* sc);
+    MOMOSA_DLL_API SearchContext* create(const Point* points_begin, const Point* points_end);
+    MOMOSA_DLL_API int32_t search(SearchContext* sc, const Rect rect, const int32_t count, Point* out_points);
+    MOMOSA_DLL_API SearchContext* destroy(SearchContext* sc);
 }
